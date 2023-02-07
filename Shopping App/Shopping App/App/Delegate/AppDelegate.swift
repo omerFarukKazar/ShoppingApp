2f7270d5bebb6a9f9345c425db7fa424b307b187
omerfkzr <omer.kazar22@gmail.com>
2023-02-07 18:12:13 +0300

    Folder Hierarchy Changed. Storyboard deleted, Programmatic UI settings have been made.
    Project divided into two main folders: App and Scenes.
    Everything related to App in general resides in App Folder.
    
    Storyboard related plist properties and files deleted.
    window property defined and set in AppDelegate by setWindow() function.

---
+++ Shopping App/Shopping App/App/Delegate/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 7.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? // Created window here because it is deleted with the SceneDelegate.swift file.
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        setWindow()
+        return true
+    }
+
+    /// Does the necessarry settings for window to present the viewController properly.
+    func setWindow() {
+        let window = UIWindow(frame: UIScreen.main.bounds) // Created a temporary window and defined the bounds.
+        let viewController = vc() // Got an instance of the first screen's ViewController.
+        let navigationController = UINavigationController(rootViewController: viewController) // Embedded view controller into a navigation controller.
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window // Assigned the temporary window to AppDelegate's window.
+    }
+}
+
---
+++ Shopping App/Shopping App/Screens/Trial.swift
@@ -0,0 +1,17 @@
+//
+//  Trial.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 7.02.2023.
+//
+
+import UIKit
+
+// This is a temporary ViewController. Created to try if no storyboard settings done correctly or not.
+
+final class vc: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
