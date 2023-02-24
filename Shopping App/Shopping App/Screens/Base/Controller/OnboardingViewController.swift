058edb0c0646ee4eb14de015266f6f3be8eaa859
omerfkzr <omer.kazar22@gmail.com>
2023-02-25 00:15:56 +0300

    Set Each pageView's Constraint and Layout Add pageCount to keep count the number of pages at OnboardingViewModel. Add 'setLayout()' function in order to set constraints due to page number.

--- Shopping App/Shopping App/Screens/Base/Controller/OnboardingViewController.swift
+++ Shopping App/Shopping App/Screens/Base/Controller/OnboardingViewController.swift
@@ -12,8 +12,12 @@ final class OnboardingViewController: UIViewController {
     // MARK: - Properties
     var onboardingView = OnboardingView()
     private let viewModel: OnboardingViewModel // Not used as an instance. That property could've been deleted.
-    private var pagesArray: [OnboardingPageView] = []
-
+    private var pageCount = OnboardingViewModel.Page.allCases.count
+    private var pagesArray: [OnboardingPageView] = [] {
+        didSet {
+            setLayout()
+        }
+    }
     // MARK: - Init
     init(viewModel: OnboardingViewModel) {
         self.viewModel = viewModel
@@ -36,6 +40,48 @@ final class OnboardingViewController: UIViewController {
     /// Sets the ViewController related settings of view.
     func setupView() {
         view = onboardingView
+    }
+
+    /// Sets the ScrollView's constraints(named as contentView) with respect to number of pages.
+    func setLayout() {
+        let screenWidth = UIScreen.main.bounds.width
+        let numberOfPages = pagesArray.count
+        let contentView = onboardingView.scrollView
+
+        contentView.contentSize.width = CGFloat(numberOfPages) * screenWidth
+        onboardingView.pageControl.numberOfPages = numberOfPages
+
+        guard let page = pagesArray.last else {
+            fatalError("OnboardingView not found.")
+        }
+
+        contentView.addSubview(page)
+        page.translatesAutoresizingMaskIntoConstraints = false
+
+        // Sets the constraints of multiple pages. Since setLayout() function is called each time a new page is added to pagesArray, all pages' constraints set one by one.
+        if numberOfPages == 1 { // Constraints for the first page.
+            NSLayoutConstraint.activate([
+                page.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                page.topAnchor.constraint(equalTo: contentView.topAnchor),
+                page.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+                page.widthAnchor.constraint(equalToConstant: screenWidth)
+            ])
+        } else if numberOfPages != pageCount && numberOfPages != 1 { // Constraints for the pages between first and last.
+            NSLayoutConstraint.activate([
+                page.leadingAnchor.constraint(equalTo: pagesArray[numberOfPages - 2].trailingAnchor),
+                page.topAnchor.constraint(equalTo: contentView.topAnchor),
+                page.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+                page.widthAnchor.constraint(equalToConstant: screenWidth)
+            ])
+        } else { // Constraints for the last page.
+            NSLayoutConstraint.activate([
+                page.leadingAnchor.constraint(equalTo: pagesArray[numberOfPages - 2].trailingAnchor),
+                page.topAnchor.constraint(equalTo: contentView.topAnchor),
+                page.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+                page.widthAnchor.constraint(equalToConstant: screenWidth),
+                page.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+                ])
+        }
     }
 
     /// Initiates pageview's with the data on Page enum. Then appends pages to the pagesArray one by one.
