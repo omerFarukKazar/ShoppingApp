0d049c5cfdd68a71eb2a651e2d676bd068fb6426
omerfkzr <omer.kazar22@gmail.com>
2023-03-08 07:46:57 +0300

    Add necessary enums to AuthViewModel In order to avoid typo

--- Shopping App/Shopping App/Screens/Authorization/ViewModel/AuthViewModel.swift
+++ Shopping App/Shopping App/Screens/Authorization/ViewModel/AuthViewModel.swift
@@ -9,33 +9,27 @@ import Foundation
 
 final class AuthViewModel {
 
+    /// Auth
+    enum AuthOption: String {
+        case logIn
+        case signUp
+    }
+
     /// Placeholder Strings for Text Fields.
     enum Placeholders: String, RawRepresentable {
         case username = "Username"
         case email = "E-mail"
         case password = "Password"
         case passwordAgain = "Re-Type your password"
+        case birthday = "Birthday"
     }
 
     enum ButtonTitles: String, RawRepresentable {
         case forgotPasswordButton = "Forgot password?"
+        case dontYouHaveAnAccountButton = "Don't you have an account?"
+        case alreadyHaveAnAccountButton = "Already have an account?"
         case logInButton = "Log In"
         case signUpButton = "Sign Up"
-    }
-
-    /// Auth
-    enum AuthOption: String {
-        case logIn
-        case signUp
-
-        var title: String {
-            switch self {
-            case .logIn:
-                return "Log In"
-            case .signUp:
-                return "Sign Up"
-            }
-        }
     }
 
 }
