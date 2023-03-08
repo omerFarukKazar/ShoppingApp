f7683ab8fd1848aab9520dcdb1313f9800230103
omerfkzr <omer.kazar22@gmail.com>
2023-03-08 07:44:38 +0300

    AuthViewModel Dependency Injection Inject AuthViewModel to AuthViewController Add missing viewModel parameter where AuthViewController instantiated.

--- Shopping App/Shopping App/Screens/Authorization/Controller/AuthViewController.swift
+++ Shopping App/Shopping App/Screens/Authorization/Controller/AuthViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 final class AuthViewController: UIViewController {
 
+    // MARK: - Properties
+    private let viewModel: AuthViewModel
+
+    // MARK: - Init
+    init(viewModel: AuthViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle Method
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view = AuthView()
-        view.backgroundColor = .white
     }
 
 }
