d9149281271982508de6fa584fffe8c70216083b
omerfkzr <omer.kazar22@gmail.com>
2023-03-08 08:00:45 +0300

    Add Views, Set Button Targets Instantiate LogInView and SignUpView Add 'switchToLogInView()' and 'switchToSignUpView()' functions to set the next view that'll be loaded if button tapped. Add button targets for the buttons to navigate between views.

--- Shopping App/Shopping App/Screens/Authorization/Controller/AuthViewController.swift
+++ Shopping App/Shopping App/Screens/Authorization/Controller/AuthViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class AuthViewController: UIViewController {
 
     // MARK: - Properties
+    private let logInView = LogInView()
+    private let signUpView = SignUpView()
     private let viewModel: AuthViewModel
 
     // MARK: - Init
@@ -26,6 +28,31 @@ final class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        switchToLogInView()
     }
 
+    // MARK: - Methods
+    /// Changes the 'self.view' to 'logInView' and does the necessary preparations.
+    private func switchToLogInView() {
+        logInView.createNewAccountButton.addTarget(nil,
+                                                   action: #selector(createNewAccountButtonTapped), for: .allEvents)
+        self.view = logInView
+    }
+
+    /// Changes the 'self.view' to 'signUpView' and does the necessary preparations.
+    private func switchToSignUpView() {
+        signUpView.alreadyHaveAnAccountButton.addTarget(nil,
+                                                        action: #selector(alreadyHaveAnAccountButtonTapped), for: .allEvents)
+        self.view = signUpView
+    }
+
+    /// Selector method of createNewAccountButton
+    @objc private func createNewAccountButtonTapped() {
+        switchToSignUpView()
+    }
+
+    /// Selector method of alreadyHaveAnAccountButton.
+    @objc private func alreadyHaveAnAccountButtonTapped() {
+        switchToLogInView()
+    }
 }
