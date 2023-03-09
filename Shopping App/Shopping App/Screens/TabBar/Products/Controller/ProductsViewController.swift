cc0e637d154eb1fd8917acd36b94ffe20525f9fa
omerfkzr <omer.kazar22@gmail.com>
2023-03-10 00:52:31 +0300

    Create TabBar Files & File Hierarchy. Create UIViewController for each tab.

---
+++ Shopping App/Shopping App/Screens/TabBar/Products/Controller/ProductsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsViewController.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 9.03.2023.
+//
+
+import UIKit
+
+final class ProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+    }
+
+}
---
+++ Shopping App/Shopping App/Screens/TabBar/Profile/Controller/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 9.03.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+    }
+
+}
