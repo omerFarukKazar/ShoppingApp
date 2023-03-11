6e3e9288fc8d02155f092725e955a831306f4845
omerfkzr <omer.kazar22@gmail.com>
2023-03-12 01:26:08 +0300

    Create Network Folder inside Core Folder Create HTTPMethod file Define enum for HTTP Request methods.

---
+++ Shopping App/Shopping App/App/Core/Network/Base/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  RequestMethod.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 11.03.2023.
+//
+
+/// HTTP request methods
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
