f1306530724a148a1f58b27a6edf69daeabef634
omerfkzr <omer.kazar22@gmail.com>
2023-03-16 02:23:11 +0300

    Add 'downloadImageData' func to ProductsViewModel
    In order to download image data and pass that data to ProductsViewController.

--- Shopping App/Shopping App/Screens/TabBar/Products/ViewModel/ProductsViewModel.swift
+++ Shopping App/Shopping App/Screens/TabBar/Products/ViewModel/ProductsViewModel.swift
@@ -54,6 +54,37 @@ final class ProductsViewModel {
         })
     }
 
+    /// Takes a url input and sends dataTask request to that url.
+    /// Handles the possible outcomes with completion.
+    /// - parameters:
+    ///     - urlString: String of the url for the desired image data.
+    ///     - completion: Closure to handle data or error values.
+    func downloadImageData(_ urlString: String,
+                           completion: ((_ imageData: Data?, _ error: Error?) -> Void)? ) {
+        guard let url = URL(string: urlString) else {
+            completion?(nil, NSError(domain: "fakestore.api", code: 400))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion?(nil, error)
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else { return }
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion?(nil, NSError(domain: "fakestore.api", code: httpResponse.statusCode))
+                return
+            }
+
+            if let data = data {
+                completion?(data, nil)
+                return
+            }
+        }.resume()
+    }
+
     /// Fetch user's favorite products list from Firestore Database.
     /// If fetch failures, trigger the 'delegate.didErrorOccured'
     /// If fetch is success, assign that data to 'favorites' array in viewModel.
