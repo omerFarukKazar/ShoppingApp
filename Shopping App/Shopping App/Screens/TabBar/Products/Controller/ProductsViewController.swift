248707e2493304bfdaca20d19144ce7fdd431e17
omerfkzr <omer.kazar22@gmail.com>
2023-03-18 23:15:09 +0300

    Update ProductsViewController.swift
    Add 'isCartEmpty' property to toggle cart button image
    Add selectedCellIndexPath to reload that cell in viewWillAppear.
    Use viewWillAppear to reload cell in case of some changes in ProductDetailView made.
    Add CartButton functions

--- Shopping App/Shopping App/Screens/TabBar/Products/Controller/ProductsViewController.swift
+++ Shopping App/Shopping App/Screens/TabBar/Products/Controller/ProductsViewController.swift
@@ -11,6 +11,13 @@ final class ProductsViewController: SAViewController {
     // MARK: - Properties
     var collectionView: UICollectionView!
     let viewModel: ProductsViewModel
+    var isCartEmpty: Bool = true {
+        didSet {
+            let image = isCartEmpty ? UIImage(named: ProductViewAssets.cart.rawValue) : UIImage(named: ProductViewAssets.cartCheckout.rawValue)
+            navigationBarCartButton(with: image)
+        }
+    }
+    var selectedCellIndexPath: IndexPath?
 
     // MARK: - Init
     init(viewModel: ProductsViewModel) {
@@ -25,14 +32,40 @@ final class ProductsViewController: SAViewController {
     // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Products"
+
         setCollectionView()
         setCollectionViewDelegateAndDataSource()
         setProductsViewModelDelegate()
-        viewModel.fetchProducts()
         viewModel.fetchFavoritesFromDB()
+        viewModel.fetchProducts()
+    }
+
     }
 
     // MARK: - Methods
+    // MARK: CartButton
+    /// Sets navigation cart button with the given image.
+    private func navigationBarCartButton(with image: UIImage?) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: image,
+            style: .plain,
+            target: self,
+            action: #selector(cartButtonTapped))
+    }
+
+    /// set and push CartViewController()
+    @objc private func cartButtonTapped() {
+        let viewModel = CartViewModel()
+        let viewController = CartViewController(viewModel: viewModel)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+
+    private func updateIsCartEmptyProperty() {
+        isCartEmpty = ProductsManager.cart.isEmpty
+    }
+
+    // MARK: 'sets'
     /// Instantiates and sets a UICollectionViewFlowLayout, uses it to create a CollectionView.
     /// Assigns that collectionview to self.view
     private func setCollectionView() {
@@ -50,24 +83,25 @@ final class ProductsViewController: SAViewController {
         self.view = collectionView
     }
 
-    /// Sets collectionView's delegate and data source to self.
     private func setCollectionViewDelegateAndDataSource() {
         collectionView.dataSource = self
         collectionView.delegate = self
     }
 
+    // TODO: I'm aware that i use 'set' a lot. But i'm open to adapt the company's lingo.
     private func setProductsViewModelDelegate() {
         viewModel.delegate = self
     }
 
 }
-
+// MARK: - UICollectionViewDelegate
 extension ProductsViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     }
 
 }
 
+// MARK: - UICollectionViewDataSource
 extension ProductsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.products.count
@@ -75,10 +109,12 @@ extension ProductsViewController: UICollectionViewDataSource {
 
     // swiftlint:disable:next line_length
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        // Instantiate cell and product
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ProductsViewCell
         guard let cell = cell else { fatalError("ProductsViewCell can not found.") }
         guard let product = viewModel.productFor(indexPath) else {
-            fatalError("Product could not found.")
+            fatalError("Product not found.")
         }
 
         // Unwrap product properties and shorten their names.
@@ -93,7 +129,7 @@ extension ProductsViewController: UICollectionViewDataSource {
         cell.priceLabel.text = "$ \(price)"
 
         // Download image with product's url
-        viewModel.downloadImageData(imageUrl) { imageData, error in
+        viewModel.downloadImageWith(imageUrl) { imageData, error in
             if let error = error {
                 self.didErrorOccured(error)
             }
@@ -101,15 +137,18 @@ extension ProductsViewController: UICollectionViewDataSource {
             cell.productImage = UIImage(data: data)
         }
 
-        // Fetch the latest favorite items from DB
-        viewModel.fetchFavoritesFromDB()
-        cell.isFavorite = viewModel.favorites.contains(id)
+        // Check if cell is favorited.
+        cell.isFavorite = ProductsManager.favorites.contains(id)
 
-        // This can be added to UIColor as an extension. UIColor(rate: ) -> UIColor
+        // This can be added to UIColor as an extension to avoid code repetition. UIColor(rate: ) -> UIColor
         let rgbaValues = viewModel.setRatingViewBackgroundColor(withRespectTo: rate)
-        cell.ratingStackView.backgroundColor = UIColor(red: rgbaValues[0], green: rgbaValues[1], blue: rgbaValues[2], alpha: rgbaValues[3])
+        cell.ratingStackView.backgroundColor = UIColor(red: rgbaValues[0],
+                                                       green: rgbaValues[1],
+                                                       blue: rgbaValues[2],
+                                                       alpha: rgbaValues[3])
 
-        // If favorite button is triggered, this closure will work for that cell.
+        // When favorite button is tapped, this closure will work for that cell.
+        // Adds or removes product according to the state of cell's isFavorite property.
         cell.didTapFavoriteButton = {
             if cell.isFavorite {
                 self.viewModel.removeFromFavoritesWith(id: id) {
@@ -142,3 +181,6 @@ extension ProductsViewController: ProductsViewModelDelegate {
         self.showAlert(title: "Error", message: error.localizedDescription)
     }
 }
+
+// MARK: - FirestoreReadAndWritable
+extension ProductsViewController: FirestoreReadAndWritable {}
