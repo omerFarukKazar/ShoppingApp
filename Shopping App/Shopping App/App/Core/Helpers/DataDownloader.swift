7d975983057bcbdc638a39e3ab3ba9eda0ccd4d7
omerfkzr <omer.kazar22@gmail.com>
2023-03-18 22:40:35 +0300

    Create DataDownloader.swift
    A protocol to avoid code repetition while downloading data from the given url.
    First created for downloading image data but it can be generalized and used for other download operations.

---
+++ Shopping App/Shopping App/App/Core/Helpers/DataDownloader.swift
@@ -0,0 +1,46 @@
+//
+//  DataDownloader.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 18.03.2023.
+//
+
+import Foundation
+
+protocol DataDownloader {
+    func downloadImageData(_ urlString: String,
+                           completion: ((_ imageData: Data?, _ error: Error?) -> Void)? )
+}
+
+extension DataDownloader {
+    /// Takes a url input and sends dataTask request to that url.
+    /// Handles the possible outcomes with completion.
+    /// - parameters:
+    ///     - urlString: String of the url for the desired image data.
+    ///     - completion: Closure to handle data or error values.
+    func downloadImageData(_ urlString: String,
+                           completion: ((_ imageData: Data?, _ error: Error?) -> Void)? ) {
+        guard let url = URL(string: urlString) else {
+            completion?(nil, NSError(domain: "fakestore.api", code: 400))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion?(nil, error)
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else { return }
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion?(nil, NSError(domain: "fakestore.api", code: httpResponse.statusCode))
+                return
+            }
+
+            if let data = data {
+                completion?(data, nil)
+                return
+            }
+        }.resume()
+    }
+}
