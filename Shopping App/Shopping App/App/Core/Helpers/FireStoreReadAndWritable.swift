a856d81d406539737e0bacee30952d364c59bf66
omerfkzr <omer.kazar22@gmail.com>
2023-03-19 23:11:02 +0300

    Fix secure entry button and some Helpers

--- Shopping App/Shopping App/App/Core/Helpers/FireStoreReadAndWritable.swift
+++ Shopping App/Shopping App/App/Core/Helpers/FireStoreReadAndWritable.swift
@@ -21,21 +21,24 @@ protocol FirestoreReadAndWritable: FireBaseFireStoreAccessible,
     func removeProductFrom(documentPath: String, withId id: Int?, completion: @escaping ((_ error: Error?) -> Void))
 }
 
-// TODO: Maybe some properties can be defined in protocol ( like "users" ) and,
+// TODO: Maybe some properties can be defined in protocol ( like "users" ) and
 // a data type as a type of enum for the document type like array, int, string and returns the related function to user.
 // That's how unexpected problems will be avoided.
 
 extension FirestoreReadAndWritable {
-    /// Accesses FireStore database with user's uid stored in defaults and **removes** the product's id from corresponding **array** that is stored in firestore db.
+    /// Accesses FireStore database with user's uid stored in defaults and **removes** the product's id from
+    /// corresponding **array** that is stored in firestore db.
     /// - parameters:
     ///     - documentPath:: Name of the document in FireStore
     ///     - id:: The id of the product whose Favorite button was tapped on.
     ///     - completion:: To handle possible outcomes.
-    /// - warning: This function's collection is **"users"** and only adds or removes the id parameter to the **array** in Firebase.
+    /// - warning: This function's collection is **"users"** and
+    /// only adds or removes the id parameter to the **array** in Firebase.
     ///
-    func addProductTo(documentPath: FirestoreDocumentPath.RawValue, withId id: Int?, completion: @escaping ((_ error: Error?) -> Void)) {
+    func addProductTo(documentPath: FirestoreDocumentPath.RawValue,
+                      withId id: Int?,
+                      completion: @escaping ((_ error: Error?) -> Void)) {
         let uid = UserDefaults.standard.string(forKey: "uid")
-        let db = Firestore.firestore()
         guard let id = id,
               let uid = uid else { return }
         db.collection("users").document(uid).updateData([documentPath: FieldValue.arrayUnion([id])]) { error in
@@ -48,13 +51,17 @@ extension FirestoreReadAndWritable {
         }
     }
 
-    /// Accesses FireStore database with user's uid stored in defaults and **removes** the product's id from corresponding **array** that is stored in firestore db.
+    /// Accesses FireStore database with user's uid stored in defaults and **removes** the product's id from
+    /// corresponding **array** that is stored in firestore db.
     /// - parameters:
     ///     - documentPath:: Name of the document in FireStore
     ///     - id:: The id of the product whose Favorite button was tapped on.
     ///     - completion:: To handle possible outcomes.
-    /// - warning: This function's collection is **"users"** and only adds or removes the id parameter to the **array** in Firebase.
-    func removeProductFrom(documentPath: FirestoreDocumentPath.RawValue, withId id: Int?, completion: @escaping ((_ error: Error?) -> Void)) {
+    /// - warning: This function's collection is **"users"** and only
+    /// adds or removes the id parameter to the **array** in Firebase.
+    func removeProductFrom(documentPath: FirestoreDocumentPath.RawValue,
+                           withId id: Int?,
+                           completion: @escaping ((_ error: Error?) -> Void)) {
         guard let id = id,
               let uid = uid else { return }
         db.collection("users").document(uid).updateData([documentPath: FieldValue.arrayRemove([id])]) { error in
@@ -69,12 +76,14 @@ extension FirestoreReadAndWritable {
 
 }
 
-// TODO: Maybe some properties can be defined in protocol ( like "users" ) and a data type as a type of enum for the document type like array, int, string and returns the related function to user.
-//enum FirestoreOperation {
+// TODO: Maybe some properties can be defined in protocol ( like "users" )
+// and a data type as a type of enum for the document type like array,
+// int, string and returns the related function to user.
+// enum FirestoreOperation {
 //    case add
 //    case remove
-//}
+// }
 //
-//enum FirestoreCollections: String {
+// enum FirestoreCollections: String {
 //    case users
-//}
+// }
