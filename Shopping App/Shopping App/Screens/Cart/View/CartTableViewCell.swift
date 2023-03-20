63d3f9e34d87bbd3abf677c9be5030de7995489b
omerfkzr <omer.kazar22@gmail.com>
2023-03-20 05:36:36 +0300

    Update project.pbxproj
    Add trash icon asset
    Change CartView.swift file's name to CartTableViewCell.
    Change the class type to UITableViewCell

---
+++ Shopping App/Shopping App/Screens/Cart/View/CartTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CartView.swift
+//  Shopping App
+//
+//  Created by Ömer Faruk Kazar on 17.03.2023.
+//
+
+import UIKit
+
+enum CartIcons: String {
+    case trash
+}
+
+final class CartTableViewCell: UITableViewCell {
+
+    // MARK: UI Elements
+}
