20badb2f48302ce6f829bd4140b945cb69fba00b
omerfkzr <omer.kazar22@gmail.com>
2023-03-20 05:40:03 +0300

    Update CartTableViewCell.swift
    Create the UI Elements of CartTableViewCell
    Add Init methods

--- Shopping App/Shopping App/Screens/Cart/View/CartTableViewCell.swift
+++ Shopping App/Shopping App/Screens/Cart/View/CartTableViewCell.swift
@@ -14,4 +14,46 @@ enum CartIcons: String {
 final class CartTableViewCell: UITableViewCell {
 
     // MARK: UI Elements
+    private let productImageView: UIImageView = {
+        let productImageView = UIImageView()
+        return productImageView
+    }()
+
+    private let productNameLabel: UILabel = {
+        let productNameLabel = UILabel()
+        productNameLabel.numberOfLines = 2
+        productNameLabel.font = .systemFont(ofSize: 16)
+        return productNameLabel
+    }()
+
+    private let productPriceLabel: UILabel = {
+        let productPriceLabel = UILabel()
+        productPriceLabel.font = .systemFont(ofSize: 18)
+        return productPriceLabel
+    }()
+
+    private let removeButton: UIButton = {
+        let removeButton = UIButton()
+        removeButton.setImage(UIImage(named: CartIcons.trash
+            .rawValue), for: .normal)
+        return removeButton
+    }()
+
+    private let stepper: UIStepper = {
+        let stepper = UIStepper()
+        return stepper
+    }()
+
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+     }
+
+    // MARK: - Methods
+
 }
