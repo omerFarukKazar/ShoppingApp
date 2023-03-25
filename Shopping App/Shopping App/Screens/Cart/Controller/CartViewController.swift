02917342f6fcd30cb55a9f3c5b42393a9b100fcb
omerfkzr <omer.kazar22@gmail.com>
2023-03-25 03:32:00 +0300

    Update CartViewController.swift
    Set the each table view cell
    Get the product details and download products images

--- Shopping App/Shopping App/Screens/Cart/Controller/CartViewController.swift
+++ Shopping App/Shopping App/Screens/Cart/Controller/CartViewController.swift
@@ -59,6 +59,43 @@ extension CartViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CartTableViewCell
         guard let cell = cell else { fatalError("Cell not found!") }
+        let keys = Array(ProductsManager.cart.keys)
+        let idArray = keys.sorted()
+
+        guard let products = products,
+              let imageUrl = products[indexPath.row].image else { return cell }
+        let product = products[indexPath.row]
+        var productImage: UIImage?
+
+        viewModel.downloadImageWith(imageUrl) { data, error in
+            if let error = error {
+                self.showError(error)
+            }
+            guard let data = data else { return }
+            productImage = UIImage(data: data)
+        }
+
+        DispatchQueue.main.async {
+            cell.productPriceLabel.text = "\(product.price ?? .zero)"
+            cell.productImageView.image = productImage
+            cell.productNameLabel.text = "\(product.title ?? "-")"
+            cell.stepperLabel.text = "\(ProductsManager.cart[indexPath.row] ?? .zero)"
+        }
+
+                viewModel.getSingleProduct(with: idArray[indexPath.row]) { product, error in
+                    if let error = error {
+                        self.showError(error)
+                    } else {
+                        guard let product = product else { return }
+        
+                        DispatchQueue.main.async {
+                            cell.productNameLabel.text = product.title
+                            cell.stepperLabel.text = "\(ProductsManager.cart[product.id ?? 0] ?? 0)"
+                            cell.productPriceLabel.text = "\(product.price ?? 0)"
+                        }
+                    }
+                }
+
         cell.backgroundColor = .lightGray
         return cell
     }
