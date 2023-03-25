3cca4ee5043f5865b993120681da04192f789645
omerfkzr <omer.kazar22@gmail.com>
2023-03-25 03:15:55 +0300

    Update ProductsManager
    Add 'fetchCart' function to fetch the items in the cart from server and synchronize the static cart variable with that.
    Add 'fetchFavoritesFromDB' function to fetch the items in the cart from server and synchronize the static favorites variable with that.
    
    Update User model to meet the Cart's data type

--- Shopping App/Shopping App/App/Core/Helpers/ProductsManager.swift
+++ Shopping App/Shopping App/App/Core/Helpers/ProductsManager.swift
@@ -11,4 +11,45 @@ final class ProductsManager {
     static let products = ProductsManager()
     static var cart: [Int: Int] = [:]
     static var favorites: [Int] = []
+
+}
+
+extension ProductsManager: FireBaseFireStoreAccessible,
+                           UserDefaultsAccessible {
+
+    func fetchCart(completion: @escaping ((_ error: Error?) -> Void) ) {
+        guard let uid = uid else { return }
+        db.collection("users").document(uid).getDocument { snapshot, error in
+            if let error = error {
+                completion(error)
+                return
+            } else {
+                guard let data = snapshot?.data(),
+                      let cartData = data["cart"] as? Data else { return }
+                do {
+                    let cart = try JSONDecoder().decode(Dictionary<Int, Int>.self,
+                                                        from: cartData)
+                    ProductsManager.cart = cart
+                } catch {
+                    completion(error)
+                }
+            }
+        }
+    }
+
+    func fetchFavoritesFromDB(completion: @escaping ((_ error: Error?) -> Void)) {
+        guard let uid = uid else { return }
+        db.collection("users").document(uid).getDocument { snapshot, error in
+            if let error = error {
+                completion(error)
+                return
+            } else {
+                if let data = snapshot?.data() {
+                    let dataAsInt = data["favorites"] as? [Int]
+                    guard let dataAsInt = dataAsInt else { return }
+                    ProductsManager.favorites = dataAsInt
+                }
+            }
+        }
+    }
 }
--- Shopping App/Shopping App/Screens/Authorization/Model/User.swift
+++ Shopping App/Shopping App/Screens/Authorization/Model/User.swift
@@ -12,7 +12,7 @@ struct User: Codable {
     let email: String?
     let birthday: String?
     var favorites: [Int]? = []
-    var cart: [Int]? = []
+    var cart: [Int: Int]? = [:]
 }
 
 extension User {
@@ -23,6 +23,6 @@ extension User {
         email = dict["email"] as? String
         birthday = dict["birthday"] as? String
         favorites = dict["favorites"] as? [Int]
-        cart = dict["cart"] as? [Int]
+        cart = dict["cart"] as? [Int: Int]
     }
 }
