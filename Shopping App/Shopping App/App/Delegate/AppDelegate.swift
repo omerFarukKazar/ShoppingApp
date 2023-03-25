53759669ceda704000ff7fb82a8629d83e9a4b9c
omerfkzr <omer.kazar22@gmail.com>
2023-03-25 03:18:05 +0300

    Update AppDelegate.swift
    Add 'syncCart' method to sync client's cart property with server when app is first opened.
    Add 'syncFavorites' method to sync client's favorites property with server when app is first opened.

--- Shopping App/Shopping App/App/Delegate/AppDelegate.swift
+++ Shopping App/Shopping App/App/Delegate/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // swiftlint:disable:next line_length
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        syncCart()
+        syncFavorites()
         // Override point for customization after application launch.
         // swiftlint:disable:next void_function_in_ternary
         UserDefaults.standard.bool(forKey: "didOnboardingCompleted") ? setWindow() : setWindowForFirstRun()
@@ -25,10 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     /// Does the necessarry settings for window to present the viewController properly.
-    func setWindow() {
+    private func setWindow() {
         let window = UIWindow(frame: UIScreen.main.bounds) // Created a temporary window and defined the bounds.
         let viewModel = AuthViewModel()
         let viewController = AuthViewController(viewModel: viewModel)
+        //        let viewModel = CartViewModel()
+        //        let viewController = CartViewController(viewModel: viewModel)
         // Got an instance of the first screen's ViewController.
         let navigationController = UINavigationController(rootViewController: viewController)
         // Embedded view controller into a navigation controller.
@@ -38,12 +42,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     /// Does the necessarry settings for window to present the viewController if onboarding isn't completed before.
-    func setWindowForFirstRun() {
+    private func setWindowForFirstRun() {
         let window = UIWindow(frame: UIScreen.main.bounds)
         let viewController = OnboardingViewController(viewModel: OnboardingViewModel())
         let navigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
+    }
+
+    private func syncCart() {
+        ProductsManager().fetchCart { error in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+        }
+    }
+    private func syncFavorites() {
+        ProductsManager().fetchFavoritesFromDB { error in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+        }
     }
 }
