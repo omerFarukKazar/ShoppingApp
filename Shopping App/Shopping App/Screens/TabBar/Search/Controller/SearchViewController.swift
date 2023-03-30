e15e0d149521301f66359c166d55a47d0fa32ab1
omerfkzr <omer.kazar22@gmail.com>
2023-03-30 05:43:03 +0300

    Update SearchViewController.swift
    Assign searchView to view.
    Add CollectionView's delegate and data source with 'setCollectionViewDelegateAndDataSource' method. -> Conform the Data Source delegate in the extensions with dummy data.
    Add and set search bar in the 'addSearchBar' and 'setSearchBarDelegate' methods.

--- Shopping App/Shopping App/Screens/TabBar/Search/Controller/SearchViewController.swift
+++ Shopping App/Shopping App/Screens/TabBar/Search/Controller/SearchViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 final class SearchViewController: SAViewController {
+
     // MARK: - Properties
     let viewModel: SearchViewModel
+    let searchView = SearchView()
 
     // MARK: - Init
     init(viewModel: SearchViewModel) {
@@ -24,9 +26,39 @@ final class SearchViewController: SAViewController {
     // MARK: - Lifecycle Method
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        view = searchView
+        setCollectionViewDelegateAndDataSource()
+        addSearchBar()
+        setSearchBarDelegate()
     }
 
     // MARK: - Methods
+    func addSearchBar() {
+        navigationItem.searchController = searchView.searchController
+    }
 
+    func setSearchBarDelegate() {
+        searchView.searchController.searchBar.delegate = self
+    }
+    func setCollectionViewDelegateAndDataSource() {
+        searchView.collectionView.delegate = self
+        searchView.collectionView.dataSource = self
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate { }
+
+extension SearchViewController: UICollectionViewDelegate { }
+
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ProductsViewCell else { fatalError("Cell Not Found") }
+        cell.backgroundColor = .cyan
+        return cell
+    }
 }
